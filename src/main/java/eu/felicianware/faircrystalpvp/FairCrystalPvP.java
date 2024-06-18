de3c4396507fd884827efc0e87a5e359ec64f2a8
LachCrafter <lachcrafteryt@gmail.com>
2024-06-18 20:36:34 +0200

    init

---
+++ src/main/java/eu/felicianware/faircrystalpvp/FairCrystalPvP.java
@@ -0,0 +1,17 @@
+package eu.felicianware.faircrystalpvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FairCrystalPvP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
