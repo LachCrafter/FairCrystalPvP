8f7316803454e25aa23de96a5bce2d306b376556
LachCrafter <lachcrafteryt@gmail.com>
2024-08-04 22:46:00 +0200

    update

--- src/main/java/eu/felicianware/faircrystalpvp/FairCrystalPvP.java
+++ src/main/java/eu/felicianware/faircrystalpvp/FairCrystalPvP.java
@@ -1,19 +1,21 @@
 package eu.felicianware.faircrystalpvp;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
+import org.bukkit.entity.EnderCrystal;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 public final class FairCrystalPvP extends JavaPlugin implements Listener {
+
 
     @Override
     public void onEnable() {
-        // Registering events
         getServer().getPluginManager().registerEvents(this, this);
         getLogger().info("FairCrystalPvP has been enabled!");
     }
@@ -25,22 +27,17 @@ public final class FairCrystalPvP extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getMaterial().isBlock()) {
-            Block clickedBlock = event.getClickedBlock();
-            if (event.getMaterial().equals(Material.END_CRYSTAL)) {
-                long placeTime = System.currentTimeMillis();
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getItem() != null && event.getItem().getType() == Material.END_CRYSTAL) {
+        }
+    }
 
-                Bukkit.getScheduler().runTaskLater(this, () -> {
-                    assert clickedBlock != null;
-                    if (clickedBlock.getType().equals(Material.END_CRYSTAL)) {
-                        long breakTime = System.currentTimeMillis();
-                        long timeDifference = breakTime - placeTime;
-
-                        if (timeDifference > 250) { // 250ms = 5 ticks
-                            event.setCancelled(true);
-                        }
-                    }
-                }, 5L);
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if (event.getEntityType() == EntityType.ENDER_CRYSTAL) {
+            EnderCrystal crystal = (EnderCrystal) event.getEntity();
+            int ticksLived = crystal.getTicksLived();
+            if (ticksLived < 3) {
+                event.setCancelled(true);
             }
         }
     }
